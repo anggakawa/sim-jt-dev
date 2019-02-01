a605c9544ad3ae9fcce57ff7e987261aa16cf94c
anggakawa <anggakawa@gmail.com>
2019-02-01 16:18:29 +0800

    created login function with axios and vuex

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,53 @@
+/* eslint-disable camelcase */
 import Vue from 'vue';
 import Vuex from 'vuex';
+
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    token: localStorage.getItem('user-token') || '',
+    status: '',
   },
   mutations: {
-
+    authRequest: (state) => {
+      state.status = 'loading';
+    },
+    authSuccess: (state, token) => {
+      state.status = 'success';
+      state.token = token;
+    },
+    authError: (state) => {
+      state.status = 'error';
+    },
   },
   actions: {
-
+    async login({ commit }, user) {
+      commit('authRequest');
+      const result = await axios.post('http://localhost:3000/api/token', user)
+        .then((res) => {
+          console.log(res);
+          if (res.data.success === true) {
+            const token_data = res.data.token;
+            localStorage.setItem('user-token', token_data);
+            commit('authSuccess', token_data);
+          } else {
+            commit('authError');
+          }
+          return res;
+        })
+        .catch((err) => {
+          commit('authError', err);
+          localStorage.removeItem('user-token');
+          // we need to test and handle if server is error and see how front end handle it
+        });
+      return result;
+    },
+  },
+  getters: {
+    isAuthenticated: state => !!state.token,
+    authStatus: state => state.status,
   },
 });
--- client/src/views/LoginForm.vue
+++ client/src/views/LoginForm.vue
@@ -1,27 +1,27 @@
 <template>
   <v-container fluid fill-height>
-    <v-layout align-center justify-center>
+    <v-layout align-center justify-center row wrap>
       <v-flex xs12 sm8 md4>
         <v-card class="elevation-12">
+          <div>
+            <v-alert v-model="alert" dismissible type="error">
+              {{ error_message }}
+            </v-alert>
+          </div>
           <v-toolbar dark color="primary">
             <v-toolbar-title>Login form</v-toolbar-title>
           </v-toolbar>
           <v-card-text>
             <v-form>
-              <v-text-field prepend-icon="person"
-                name="login" label="Login" type="text"></v-text-field>
-              <v-text-field
-                prepend-icon="lock"
-                name="password"
-                label="Password"
-                id="password"
-                type="password"
-              ></v-text-field>
+              <v-text-field prepend-icon="person" v-model="username" name="login" label="Login"
+                type="text"></v-text-field>
+              <v-text-field prepend-icon="lock" name="password" v-model="password" label="Password"
+                id="password" type="password"></v-text-field>
             </v-form>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="primary">Login</v-btn>
+            <v-btn color="primary" @click="login">Login</v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
@@ -30,9 +30,35 @@
 </template>
 
 <script>
-export default {
+  export default {
+    data() {
+      return {
+        username: '',
+        password: '',
+        alert: false,
+        error_message: '',
+      }
+    },
+    methods: {
+      login() {
+        const username = this.username;
+        const password = this.password;
+        this.$store.dispatch('login', {
+          username,
+          password
+        }).then((res) => {
+          if (res.data.success) {
+            this.$router.push('/');
+          } else {
+            this.error_message = res.data.result;
+            this.alert = true;
+            console.log(res.data.result);
+          }
+        }).catch((err) => console.log(err));
+      }
+    }
+  };
 
-};
 </script>
 
 <style>
