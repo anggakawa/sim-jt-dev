960896cc58ad3f25fae7359b0639a622d838d676
anggakawa <anggakawa@gmail.com>
2019-02-15 17:10:12 +0800

    minor changes

--- client/src/components/OrderTable.vue
+++ client/src/components/OrderTable.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <v-toolbar flat color="white">
-      <v-toolbar-title>My CRUD</v-toolbar-title>
+      <v-toolbar-title>Orders</v-toolbar-title>
       <v-divider class="mx-2" inset vertical></v-divider>
       <v-spacer></v-spacer>
       <v-dialog v-model="dialog" max-width="500px">
