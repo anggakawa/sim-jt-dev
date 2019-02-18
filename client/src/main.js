75268d18c0302ac3d6e6259d25f623bd9b3b46a9
anggakawa <anggakawa@gmail.com>
2019-02-18 09:42:51 +0800

    add decoded token and expired token handler

--- client/src/main.js
+++ client/src/main.js
@@ -6,13 +6,18 @@ import router from './router';
 import store from './store';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
-
+import jwtDecode from 'jwt-decode';
 
 Vue.config.productionTip = false;
 
 const token = localStorage.getItem('user-token');
 if (token) {
-  Axios.defaults.headers.common['Authorization'] = "Bearer " + token;
+  const decode = jwtDecode(token);
+  if (Date.now() / 1000 > decode.exp) {
+    localStorage.removeItem('user-token');
+  } else {
+    Axios.defaults.headers.common['Authorization'] = "Bearer " + token;
+  }
 }
 
 new Vue({
